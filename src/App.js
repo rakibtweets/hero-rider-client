34d5555f8faad36545adda61a84a507bf72f26a8
rakibtweets <rakibhasanroky0@gmail.com>
2021-12-31 17:38:50 +0600

    navigation complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import './Navigation.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div>
-      <h1>Boostrap Navigation</h1>
-    </div>
+    <Navbar collapseOnSelect expand="lg" sticky="top" className="nav-bg py-3">
+      <Container>
+        <Navbar.Brand className="me-auto" to="#home">
+          <span className="brand-logo"> Hero Rider</span>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          className="text-white"
+          aria-controls="responsive-navbar-nav"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto d-flex align-items-center">
+            <Link className="nav-item" to="/home">
+              Home
+            </Link>
+            <Link className="nav-item" to="/packages">
+              Packages
+            </Link>
+            <Link className="nav-item" to="/packages">
+              About
+            </Link>
+          </Nav>
+          <Nav className="ms-auto d-flex align-items-center ">
+            <Link className="nav-item" to="/deets">
+              Sign as Rider
+            </Link>
+            <Link className="nav-item" eventKey={2} to="/memes">
+              Sign as Learner
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
