339b96dc203eac952ac28d24b9e13cd8576a0243
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-01 12:39:17 +0600

    firebase useAuth hooks

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RiderSignUp from './Pages/Login/RiderSignUp/RiderSignUp';
+import AuthProvider from './Context/AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rider-signUp" element={<RiderSignUp />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rider-signUp" element={<RiderSignUp />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import initializeFirebase from '../Pages/Login/Firebase/firebase.init';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+initializeFirebase();
+
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [authError, setAuthError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [admin, setAdmin] = useState(false);
+
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+  return {
+    user,
+    authError,
+    isLoading,
+    admin,
+  };
+};
+
+export default useFirebase;
