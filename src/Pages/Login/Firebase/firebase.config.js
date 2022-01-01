166deb431741a76dfce752862594886c16384f4d
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-01 12:14:42 +0600

    firebase initialize

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXb9ZWke_2d2C-_FSL40n8GAZZydMcAKI',
+  authDomain: 'hero-rider-e2bb1.firebaseapp.com',
+  projectId: 'hero-rider-e2bb1',
+  storageBucket: 'hero-rider-e2bb1.appspot.com',
+  messagingSenderId: '274134310857',
+  appId: '1:274134310857:web:1e2b377b5db27769eb5218',
+};
+
+export default firebaseConfig;
